7f7760a91cc684a389970e47353f61bfec9a68d9
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-25 16:38:09 +0200

    add the paymentprocessor with a process method

--- src/PaymentProcessor.java
+++ src/PaymentProcessor.java
@@ -1,4 +1,22 @@
-public class PaymentProcessor {
+public class PaymentProcessor<T> {
+     void processPayment(Bitcoin coin) {
+         coin = new Bitcoin();
+         System.out.println(coin);
 
+    }
+    void processPayment(PayPal payPal) {
+         payPal = new PayPal();
+         System.out.println(payPal);
+
+    }
+    void ProcessPayment(CreditCard creditCard) {
+         creditCard = new CreditCard();
+        System.out.println(creditCard);
+
+    }
+    void ProcessPayment(MomoPay momoPay) {
+         momoPay = new MomoPay();
+         System.out.println(momoPay);
+    }
 
 }
