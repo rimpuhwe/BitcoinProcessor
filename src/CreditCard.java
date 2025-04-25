a99773af8ae399f2b421b01a964f3d190a6a8aa7
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-25 14:35:37 +0200

    add the implementation for the credit cards payment

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CreditCard {
+    private int id;
+    Scanner card = new Scanner(System.in);
+    CreditCard(){
+        System.out.print("Provide the credit card number");
+        id = card.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        return "Payment done with your Credit Card ";
+    }
 }
