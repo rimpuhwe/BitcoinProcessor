3ffa5046738d963891521763597944d2c7cb96a6
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-25 14:37:42 +0200

    add a momopay payment method

--- src/MomoPay.java
+++ src/MomoPay.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class MomoPay {
+    private  final int phoneNumber ;
+    MomoPay(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment goes to " + phoneNumber;
+    }
 }
